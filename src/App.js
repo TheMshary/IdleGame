3b68d834cb46f846b64b535f9cb4a859835a221d
darkwight <m.alsharekh94@gmail.com>
2018-11-29 19:19:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      HJ: 1,
+      effort: 0,
+      enlightenment: 0
+    };
+    this.effortClick = this.effortClick.bind(this);
+    this.enlighten = this.enlighten.bind(this);
+  }
+
+  effortClick() {
+    let effort = this.state.effort;
+    if (effort < 9) {
+      this.setState({ effort: effort + 1 });
+    } else {
+      this.enlighten();
+    }
+  }
+
+  enlighten() {
+    let HJ = this.state.HJ;
+    let effort = this.state.effort;
+    let enlightenment = this.state.enlightenment;
+    this.setState({
+      effort: 0,
+      HJ: HJ + 1,
+      enlightenment: enlightenment + 1
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Hero's Journey {this.state.HJ}</p>
+        <button onClick={this.effortClick}>Put In Effort</button>
+        <p>{this.state.effort}/10</p>
+        <p>Enlightenment: {this.state.enlightenment}</p>
       </div>
     );
   }
